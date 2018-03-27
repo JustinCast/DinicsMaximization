504085f0f825f522a550cdff85dfaf99663fbf73
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-03-26 23:34:53 -0600

    Métodos listos

---
+++ DinicProof/Dinic.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinicProof
+{
+    public class Dinic
+    {
+
+        // Queue for the top level BFS.
+        public LinkedList<int> q;
+
+        // Stores the graph.
+        private List<Edge>[] adj;
+        public int n;
+
+        // s = source, t = sink
+        public int s;
+        public int t;
+
+
+        // For BFS.
+        public bool[] blocked;
+        public int[] dist;
+
+        public static int oo = (int)1E9;
+
+        internal List<Edge>[] Adj { get => adj; set => adj = value; }
+
+        // Constructor.
+        public Dinic(int N)
+        {
+
+            // s is the source, t is the sink, add these as last two nodes.
+            n = N; s = n++; t = n++;
+
+            // Everything else is empty.
+            blocked = new bool[n];
+            dist = new int[n];
+            q = new LinkedList<int>();
+            Adj = new List<Edge>[n];
+            for (int i = 0; i < n; ++i)
+                Adj[i] = new List<Edge>();
+        }
+
+        // Just adds an edge and ALSO adds it going backwards.
+        public void add(int v1, int v2, int cap, int flow)
+        {
+            Edge e = new Edge(v1, v2, cap, flow);
+            Edge rev = new Edge(v2, v1, 0, 0);
+            Adj[v1].Add(rev.rev = e);
+            Adj[v2].Add(e.rev = rev);
+        }
+
+        public static void Fill<T>(T[] array, int start, int end, T value)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException("array");
+            }
+            if (start < 0 || start >= end)
+            {
+                throw new ArgumentOutOfRangeException("fromIndex");
+            }
+            //if (end >= array.Length)
+            //{
+            //    throw new ArgumentOutOfRangeException("toIndex");
+            //}
+            for (int i = start; i < end; i++)
+            {
+                array[i] = value;
+            }
+        }
+
+        // Runs other level BFS.
+        public bool bfs()
+        {
+
+            // Set up BFS
+            q.Clear();
+            Fill(dist, 0, dist.Count(), -1);
+            dist[t] = 0;
+            q.AddFirst(t);
+
+            // Go backwards from sink looking for source.
+            // We just care to mark distances left to the sink.
+            while (q.Count() != 0)
+            {
+                int node = q.ElementAt(0);
+                q.RemoveFirst();
+                if (node == s)
+                    return true;
+                for (Edge e : Adj[node])
+                {
+                    if (e.rev.cap > e.rev.flow && dist[e.v2] == -1)
+                    {
+                        dist[e.v2] = dist[node] + 1;
+                        q.add(e.v2);
+                    }
+                }
+            }
+
+            // Augmenting paths exist iff we made it back to the source.
+            return dist[s] != -1;
+        }
+
+        // Runs inner DFS in Dinic's, from node pos with a flow of min.
+        public int dfs(int pos, int min)
+        {
+
+            // Made it to the sink, we're good, return this as our max flow for the augmenting path.
+            if (pos == t)
+                return min;
+            int flow = 0;
+
+            // Try each edge from here.
+            for (Edge e : Adj[pos])
+            {
+                int cur = 0;
+
+                // If our destination isn't blocked and it's 1 closer to the sink and there's flow, we
+                // can go this way.
+                if (!blocked[e.v2] && dist[e.v2] == dist[pos] - 1 && e.cap - e.flow > 0)
+                {
+
+                    // Recursively run dfs from here - limiting flow based on current and what's left on this edge.
+                    cur = dfs(e.v2, Math.min(min - flow, e.cap - e.flow));
+
+                    // Add the flow through this edge and subtract it from the reverse flow.
+                    e.flow += cur;
+                    e.rev.flow = -e.flow;
+
+                    // Add to the total flow.
+                    flow += cur;
+                }
+
+                // No more can go through, we're good.
+                if (flow == min)
+                    return flow;
+            }
+
+            // mark if this node is now blocked.
+            blocked[pos] = flow != min;
+
+            // This is the flow
+            return flow;
+        }
+
+        public int flow()
+        {
+            clear();
+            int ret = 0;
+
+            // Run a top level BFS.
+            while (bfs())
+            {
+
+                // Reset this.
+                Arrays.fill(blocked, false);
+
+                // Run multiple DFS's until there is no flow left to push through.
+                ret += dfs(s, oo);
+            }
+            return ret;
+        }
+
+        // Just resets flow through all edges to be 0.
+        public void clear()
+        {
+            for (ArrayList<Edge> edges : Adj)
+                for (Edge e : edges)
+                    e.flow = 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace DinicsMaximization
 {
@@ -16,13 +17,71 @@ namespace DinicsMaximization
                 graph[i] = new List<Edge>();
             return graph;
         }
+
+        public static void minimunConnected(List<Edge>[] graph)
+        {
+            int size = graph.Length;
+            for (int i = 0; i < graph.Length; i++)
+            {
+                Random random = new Random();
+                int destVertex = random.Next(0, size - 1);
+                while (isConnected(graph, i, destVertex))
+                    destVertex = random.Next(0, graph.Length - 1);
+                addEdge(graph, i, destVertex, random.Next(1 , graph.Length));
+            }
+        }
+
+        public static void mediumConnected(List<Edge>[] graph)
+        {
+            Random random = new Random();
+            for (int i = 0; i < graph.Length; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    if(j != i)
+                    {
+                        if (i <= 3)
+                            addEdge(graph, i, graph.Length / 2 - (j-1), random.Next(1, graph.Length));
+                        else
+                            addEdge(graph, i, j, random.Next(1, graph.Length));
+                    }
+                    else
+                        addEdge(graph, i, j+1, random.Next(1, graph.Length));
+                }
+
+            }
+        }
+
+        public static void stronglyConnected(List<Edge>[] graph)
+        {
+            Random random = new Random();
+            for (int i = 0; i < graph.Length; i++)
+            {
+                for (int j = 0; j < graph.Length; j++)
+                {
+                    if (j != i)
+                        addEdge(graph, i, j, random.Next(1, graph.Length - 1));
+                }              
+            }
+        }
+
+        public static bool isConnected(List<Edge>[] graph, int pos, int v2)
+        {           
+            foreach (Edge e in graph[pos])
+            {
+                if (e.t == v2 || pos == v2)
+                    return true;                    
+            }
+            return false;
+        }
+
         public static void addEdge(List<Edge>[] graph, int s, int t, int cap)
         {
-            //Console.WriteLine("graph[t].Count(): " + graph[t].Count());
+            Console.WriteLine("graph[t].Count(): " + graph[t].Count());
             //Console.WriteLine("graph[s].Count() - 1: " + (graph[s].Count() - 1));
             //Console.WriteLine("||||||||||||||||||||||||||||||||||||||||||||||||");
-            graph[s].Add(new Edge(t, graph[t].Count(), cap));
-            graph[t].Add(new Edge(s, graph[s].Count() - 1, 0));
+            graph[s].Add(new Edge(t, s, cap));
+            graph[t].Add(new Edge(s, t, 0));
         }
 
         public static void Fill<T>(T[] array, int start, int end, T value)
@@ -114,37 +173,55 @@ namespace DinicsMaximization
                 - Conexión fuertemente conexa
                 - Conexión con 3 arcos por vértice
              */
-            List<Edge>[] graph = createGraph(5);
-            addEdge(graph, 0, 1, 3);
-            addEdge(graph, 0, 2, 2);
-            addEdge(graph, 1, 2, 2);
-            addEdge(graph, 0, 3, 4);
-            addEdge(graph, 1, 3, 3);
+            List<Edge>[] graph = createGraph(10);
+            //Random random = new Random();
+            //addEdge(graph, 0, 1, 3);
+            //addEdge(graph, 0, 2, 2);
+            //addEdge(graph, 1, 2, 2); ;
             //Console.WriteLine(4 == maxFlow(graph, 0, 2));
-            Console.WriteLine(maxFlow(graph, 0, 3));
+            //Console.WriteLine(maxFlow(graph, 0, 3));
+            minimunConnected(graph);
+            //Console.WriteLine(vertexConnections);
+            int count = 0;
             for (int i = 0; i < graph.Length; i++)
             {
                 foreach (Edge e in graph[i])
                 {
-                    Console.WriteLine("VALOR VERTICE 2: " + e.t);
-                    Console.WriteLine("VALOR VERTICE 1: " + e.rev);
-                    Console.WriteLine("VALOR CAPACITY: " + e.cap);
-                    Console.WriteLine("VALOR FLOW: " + e.f);
-                    Console.WriteLine("---------------------");
+                    if(e.cap != 0)
+                    {
+                        count++;
+                        Console.WriteLine(count);
+                        Console.WriteLine("VALOR VERTICE 2: " + e.t);
+                        Console.WriteLine("VALOR VERTICE 1: " + e.rev);
+                        Console.WriteLine("VALOR CAPACITY: " + e.cap);
+                        Console.WriteLine("VALOR FLOW: " + e.f);
+                        Console.WriteLine("---------------------");
+                    }
                 }
             }
-            //printMaxFlow(graph);
+            printMaxFlow(graph);
             Console.ReadKey();
         }
 
         static void printMaxFlow(List<Edge>[] graph)
         {
-            Console.WriteLine("MAXIMOS FLUJOS");
-            Console.WriteLine("Fuente = 0, Destino = 2 | " + maxFlow(graph, 0, 2));
-            Console.WriteLine("Fuente = 0, Destino = 1 | " + maxFlow(graph, 0, 1));
-            Console.WriteLine("Fuente = 1, Destino = 2 | " + maxFlow(graph, 1, 2));
-            Console.WriteLine("Fuente = 2, Destino = 3 | " + maxFlow(graph, 2, 3));
-            Console.WriteLine("Fuente = 3, Destino = 1 | " + maxFlow(graph, 3, 1));
+            Console.WriteLine("|||||| MAXIMUN FLOWS ||||||");
+            Console.WriteLine(maxFlow(graph, 0, 1));
+            Console.WriteLine(maxFlow(graph, 0, 2));
+            Console.WriteLine(maxFlow(graph, 0, 3));
+            Console.WriteLine(maxFlow(graph, 0, 4));
+            Console.WriteLine(maxFlow(graph, 0, 5));
+            Console.WriteLine(maxFlow(graph, 0, 6));
+            Console.WriteLine(maxFlow(graph, 0, 7));
+            Console.WriteLine(maxFlow(graph, 0, 8));
+            Console.WriteLine(maxFlow(graph, 0, 9));
+            //for (int i = 0; i < graph.Length; i++)
+            //{
+            //    for (int j = 0; j < graph.Length; j++) {
+            //        Console.WriteLine("IN LOOP");
+            //        Console.WriteLine(maxFlow(graph, i, j));
+            //    }
+            //}           
         }
     }
 }
